b9108dad4432b49f623f56f317ed34abf6df3713
Dave Lewis <siu06dl@gmail.com>
2024-08-15 17:36:03 +0100

    feat: expand control scheme

--- scripts/Settings/Controls.cs
+++ scripts/Settings/Controls.cs
@@ -15,6 +15,27 @@ namespace RossoSkies1.scripts.Settings
             ControllerControl = new InputBinding { Type = InputType.JoypadAxis, JoypadAxis = JoyAxis.LeftY, AxisValue = 1.0f }
         };
 
+        public ControlGroup Decelerate = new ControlGroup()
+        {
+            Name = "Decelerate",
+            KeyboardControl = new InputBinding { Type = InputType.Keyboard, Keyboard = Key.S },
+            ControllerControl = new InputBinding { Type = InputType.JoypadAxis, JoypadAxis = JoyAxis.LeftY, AxisValue = -1.0f }
+        };
+
+        public ControlGroup Right = new ControlGroup()
+        {
+            Name = "Turn Right",
+            KeyboardControl = new InputBinding { Type = InputType.Keyboard, Keyboard = Key.D },
+            ControllerControl = new InputBinding { Type = InputType.JoypadAxis, JoypadAxis = JoyAxis.LeftX, AxisValue = 1.0f }
+        };
+
+        public ControlGroup Left = new ControlGroup()
+        {
+            Name = "Turn Left",
+            KeyboardControl = new InputBinding { Type = InputType.Keyboard, Keyboard = Key.A },
+            ControllerControl = new InputBinding { Type = InputType.JoypadAxis, JoypadAxis = JoyAxis.LeftX, AxisValue = -1.0f }
+        };
+
         public ControlGroup Shoot = new ControlGroup()
         {
             Name = "Shoot Primary",
@@ -45,6 +66,9 @@ namespace RossoSkies1.scripts.Settings
         public List<ControlGroup> GetControls() =>
             new List<ControlGroup> {
                 Accelerate,
+                Decelerate,
+                Right,
+                Left,
                 Shoot,
             };
 
