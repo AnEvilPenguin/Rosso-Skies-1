0d88218e3bd59fd62238ed4b2a1570b936dc486b
Dave Lewis <siu06dl@gmail.com>
2024-08-27 20:07:26 +0100

    feat: enemy follows player

--- scripts/BasicEnemy.cs
+++ scripts/BasicEnemy.cs
@@ -2,15 +2,34 @@ using Godot;
 
 public partial class BasicEnemy : Node2D
 {
-	public Health Health;
+    [Export]
+    public float Speed = 25;
+
+    public Health Health;
+
+	private Player _player;
 
 	public override void _Ready()
 	{
 		Health = GetNode<Health>("Health");
 
+		_player = GetParent().GetNode<Player>("%Player");
+
 		Health.Destroyed += OnDestroyed;
 	}
 
-	private void OnDestroyed() =>
-		QueueFree();
+    public override void _Process(double delta)
+    {
+        // TODO bring rotation into the mix?
+        var direction = GlobalPosition.DirectionTo(_player.GlobalPosition);
+
+        
+        
+        Position += direction * (Speed * (float)delta);
+    }
+
+    private void OnDestroyed()
+	{
+        QueueFree();
+    }
 }
