2ab795a0d46bcce194fa61e80f2d54dcdc366495
Dave Lewis <siu06dl@gmail.com>
2024-08-28 18:05:08 +0100

    refactor: sepatate positioning and rotation logic

--- scripts/BasicEnemy.cs
+++ scripts/BasicEnemy.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System;
 
 public partial class BasicEnemy : Node2D
 {
@@ -20,6 +21,22 @@ public partial class BasicEnemy : Node2D
 
     public override void _Process(double delta)
     {
+        ProcessRotation();
+        ProcessPosition(delta);
+    }
+
+    private void OnDestroyed() =>
+	    QueueFree();
+
+    private void ProcessPosition(double delta)
+    {
+        var direction = new Vector2(MathF.Cos(Rotation), MathF.Sin(Rotation));
+
+        Position += direction * (Speed * (float)delta);
+    }
+
+    private void ProcessRotation()
+    {
         var direction = GlobalPosition.DirectionTo(_player.GlobalPosition);
 
         // TODO figure out how to set enemy agility
@@ -28,10 +45,5 @@ public partial class BasicEnemy : Node2D
         // TODO shoot when within a certain angle/distance of player
 
         Rotation = rotation;
-        
-        Position += direction * (Speed * (float)delta);
     }
-
-    private void OnDestroyed() =>
-	    QueueFree();
 }
