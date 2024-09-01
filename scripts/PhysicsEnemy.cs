957c53fa9eb814b16f01d0f5ff0adb795be6599b
Dave Lewis <siu06dl@gmail.com>
2024-09-01 17:53:36 +0100

    feat: initial avoidance attempt

--- scripts/PhysicsEnemy.cs
+++ scripts/PhysicsEnemy.cs
@@ -1,25 +1,39 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class PhysicsEnemy : CharacterBody2D
 {
     [Export]
     public float Speed = 25.0f;
 
+    public float CurrentSpeed;
+
     public Health Health;
 
     private Player _player;
 
     private RayCast2D _playerDetection;
+    private IEnumerable<RayCast2D> _enemyDetection; // enemy being a player enemy
     private BasicGun _gun;
+
+    private HashSet<PhysicsEnemy> _slow = new();
 
     public override void _Ready()
     {
         Health = GetNode<Health>("Health");
+
         _playerDetection = GetNode<RayCast2D>("%PlayerDetectionRayCast");
+
+        var detectionNode = GetNode<Node>("%EnemyDetection");
+        _enemyDetection = detectionNode.GetChildren().OfType<RayCast2D>();
+
         _gun = GetNode<BasicGun>("%BasicGun");
         _gun.Layer = new int[] { 4 };
         _gun.Mask = new int[] { 1 };
+
+        CurrentSpeed = Speed;
 
         _player = GetParent().GetNode<Player>("%Player");
 
@@ -33,6 +47,22 @@ public partial class PhysicsEnemy : CharacterBody2D
             // FIXME bullet layer
             _gun.Shoot(GlobalPosition.DirectionTo(_player.GlobalPosition), Rotation, Speed);
         }
+
+        _slow.Clear();
+
+        foreach (var ray in _enemyDetection)
+        {
+            if (ray.GetCollider() != null)
+            {
+                var collided = ray.GetCollider() as Node2D;
+                if (collided.GetParent() is PhysicsEnemy enemy)
+                {
+                    _slow.Add(enemy);
+                }
+            }
+        }
+
+        SetCurrentSpeed();
     }
 
     public override void _PhysicsProcess(double delta)
@@ -41,9 +71,9 @@ public partial class PhysicsEnemy : CharacterBody2D
 
         var direction = new Vector2(MathF.Cos(Rotation), MathF.Sin(Rotation));
 
-        Velocity = direction * Speed;
+        Velocity = direction * CurrentSpeed;
 
-		MoveAndSlide();
+        MoveAndSlide();
 	}
 
     private void OnDestroyed() =>
@@ -59,5 +89,19 @@ public partial class PhysicsEnemy : CharacterBody2D
         // TODO shoot when within a certain angle/distance of player
 
         Rotation = rotation;
+    }
+
+    private void SetCurrentSpeed()
+    {
+        CurrentSpeed = Speed;
+
+        if (_slow.Count == 0)
+            return;
+
+        var bottom = _slow.Aggregate(CurrentSpeed, (acc, enemy) => {
+            return Mathf.Min(acc, enemy.CurrentSpeed);
+        });
+
+        CurrentSpeed = (float)Mathf.Max(0.2, bottom * 0.8f);
     }
 }
