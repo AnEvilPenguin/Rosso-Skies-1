28391f050fc3d5d56dc6f98aea6c039db02bb85b
Dave Lewis <siu06dl@gmail.com>
2024-09-13 18:23:51 +0100

    feat: simple timer implementation

---
+++ scripts/Util/SimpleTimer.cs
@@ -0,0 +1,54 @@
+﻿using Godot;
+
+namespace RossoSkies1.scripts.Util
+{
+    internal partial class SimpleTimer : Node
+    {
+        [Signal]
+        public delegate void TimerElapsedEventHandler();
+
+        public double Time;
+
+        private double _timer;
+        private bool _started;
+
+        public override void _Process(double delta)
+        {
+            if (!_started)
+                return;
+
+            _timer -= delta;
+
+            if (_timer > 0)
+                return;
+
+            Stop();
+
+            EmitSignal(SignalName.TimerElapsed);
+        }
+
+        public void Start()
+        {
+            if (_started)
+                return;
+
+            ResetTimer();
+            _started = true;
+        }
+
+        public void Stop()
+        { 
+            _started = false;
+            ResetTimer();
+        }
+
+        public void Pause() =>
+            _started = false;
+
+        public bool IsRunning() => 
+            _started;
+
+        private void ResetTimer() =>
+            _timer = Time;
+    }
+}
